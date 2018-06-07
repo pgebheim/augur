e5451c1e8cfe5dcfcb2d73f4d1978eb72ff5be03
Paul Gebheim <paul@onetaste.us>
2018-06-07 21:45:48 +0000

    Do some smart testing

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -5,5 +5,5 @@
 set -e #exit immediately on `exit status > 0` of any command
 set -o pipefail #exit immediately on `exit status > 0` for pipes
 
-lerna run lint;
-lerna run test -- --reporter min;
+lerna run lint --since master;
+lerna run "test --reporter min"  --since master --include-filtered-dependents;
